f52056b1955798753da11f72afe020da1ae2bcf7
FrankColaco <frankcolaco@outlook.com>
2025-09-07 17:34:02 +0530

    palindromeTwoPointers

---
+++ src/com/examples/algo/twopointers/PalindromeMain.java
@@ -0,0 +1,28 @@
+package com.examples.algo.twopointers;
+public class PalindromeMain {
+    public static void main(String[] args) {
+       String s = "12321";
+       System.out.println("given string is palindrome:: "+ isPalindrome(s));
+    }
+    
+    static boolean isPalindrome(String s){
+        char[] str = s.toCharArray();
+        int left = 0;
+        int right = str.length-1;
+        
+        
+        while(left < right){
+            char l= str[left]; char r=str[right];
+            if(!Character.isLetterOrDigit(l)){
+                left++;
+            }else if(!Character.isLetterOrDigit(r)){
+                right--;
+            }else if(Character.toLowerCase(l) != Character.toLowerCase(r)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
