8c3743b5d44d82deec0bb85e4143677b2ca23c24
frankcolaco <frankcolaco@outlook.com>
2025-10-20 19:42:51 +0530

    prefix sum pattern examples with hashmap

--- src/main/java/com/examples/algo/prefixsum/SubArraySumEqKMain.java
+++ src/main/java/com/examples/algo/prefixsum/SubArraySumEqKMain.java
@@ -1,6 +1,8 @@
 package com.examples.algo.prefixsum;
 
 
+import java.util.HashMap;
+
 /*
 Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
 
@@ -16,6 +18,12 @@ public class SubArraySumEqKMain {
         System.out.println("number of subarrays whose sum is equal to 3:: "+subArraySumEqKMain.subarraySum(numArray,3));
         Integer[] numArray1 = {1,1,1};
         System.out.println("number of subarrays whose sum is equal to 3:: "+subArraySumEqKMain.subarraySum(numArray1,2));
+
+        System.out.println("::::: below result is with using hashmap ::::::");
+        Integer[] arr = {10, 2, -2, -20, 10};
+        int k = -10;
+        HashMap<Integer,Integer> prefixSums = new HashMap<>();
+        System.out.println("number of subarrays whose sum is equal to -10:: "+subArraySumEqKMain.subarraySum(arr,k,prefixSums));
 
 
     }
@@ -34,5 +42,26 @@ public class SubArraySumEqKMain {
             }
         }
     return count;
+    }
+
+    int subarraySum(Integer[] nums, int k, HashMap<Integer,Integer>prefixSums) {
+
+        int count = 0;
+        int length = nums.length;
+        int currSum = 0;
+        for(int i=0;i<length;i++){
+
+                currSum += nums[i];
+                if(currSum == k) {
+                    count++;
+                }
+
+             if(prefixSums.containsKey(currSum - k)){
+                 count +=prefixSums.get(currSum-k);
+             }
+
+             prefixSums.put(currSum,prefixSums.getOrDefault(currSum,0)+1);
+        }
+        return count;
     }
 }
